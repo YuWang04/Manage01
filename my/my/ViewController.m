80d71553b29607ee13f946f5761759a2fb030127
xiguofeng <xiguofeng@163.com>
2017-12-06 17:16:17 +0800

    加法计算

--- my/my/ViewController.m
+++ my/my/ViewController.m
@@ -10,13 +10,20 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *TextField1;
+@property (weak, nonatomic) IBOutlet UITextField *TextField2;
+
+@property (weak, nonatomic) IBOutlet UILabel *label;
+- (IBAction)AddAction:(UIButton *)sender;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.view.backgroundColor = [UIColor blueColor];
+    self.view.backgroundColor = [UIColor orangeColor];
+    
 }
 
 
@@ -26,4 +33,15 @@
 }
 
 
+- (IBAction)AddAction:(UIButton *)sender {
+    NSString *num1 = _TextField1.text;
+    NSString *num2 = _TextField2.text;
+    
+    double n1 = num1.doubleValue;
+    double n2 = [num2 doubleValue];
+    
+    double result = n1 + n2;
+    self.label.text = [NSString stringWithFormat:@"%f", result];
+    [self.view endEditing:YES];
+}
 @end
