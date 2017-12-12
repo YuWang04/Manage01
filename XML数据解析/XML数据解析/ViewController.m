e75cd89bf9b5d3c51ab01cf5586cf0bfe3da1ee0
xiguofeng <xiguofeng@163.com>
2017-12-12 10:06:50 +0800

    文件的创建，添加，删除

--- XML数据解析/XML数据解析/ViewController.m
+++ XML数据解析/XML数据解析/ViewController.m
@@ -25,21 +25,64 @@
     NSArray *docuPaths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
     NSString *documentsPath = [docuPaths lastObject];
     NSLog(@"%@",documentsPath);
-    //获取Library路径
-    NSArray *libPaths = NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES);
-    NSLog(@"libPaths = %@",libPaths);
-    //获取Tmp路径
-    NSString *tmpPath = NSTemporaryDirectory();
-    NSLog(@"tmpPath = %@",tmpPath);
-    //获取路径的各个部分
-    NSArray *arrpath = [tmpPath pathComponents];
-    NSLog(@"arrpath = %@",arrpath);
-    //删除路径的最后一个部分
-    NSString *string = [tmpPath stringByDeletingLastPathComponent];
-    NSLog(@"string = %@",string);
-    //在路径后添加一个部分
-    NSString *filepath = [tmpPath stringByAppendingString:@"name.text"];
-    NSLog(@"filepath = %@",filepath);
+    //追加文件夹路径
+    NSString *testPath = [documentsPath stringByAppendingPathComponent:@"wy"];
+    //追加文件路径
+    NSString *textFile = [testPath stringByAppendingPathComponent:@"我的文件.txt"];
+    NSFileManager *fileMng = [NSFileManager defaultManager];
+    //参数二：文件夹重复是否覆盖
+//    BOOL ret = [fileMng createDirectoryAtPath:testPath withIntermediateDirectories:NO attributes:nil error:nil];
+    BOOL ret = [fileMng createFileAtPath:textFile contents:nil attributes:nil];
+    if(ret){
+        NSLog(@"文件创建成功");
+    }else{
+        NSLog(@"文件创建失败");
+    }
+    //写入文件
+    NSString *content = @"写入到文件";
+    BOOL bl = [content writeToFile:textFile atomically:YES encoding:NSUTF8StringEncoding error:nil];
+    if(bl){
+        NSLog(@"写入成功");
+    }else{
+        NSLog(@"写入失败");
+    }
+    //判断文件是否存在
+    if([fileMng fileExistsAtPath:textFile]){
+        NSLog(@"存在");
+    }else{
+        NSLog(@"不存在");
+    }
+    //追加内容
+    //打开文件准备更新
+    NSFileHandle *fileHandle = [NSFileHandle fileHandleForUpdatingAtPath:textFile];
+    [fileHandle seekToEndOfFile];//将节点调到末尾
+    NSString *string = @"追加的内容";
+    NSData *sringData = [string dataUsingEncoding:NSUTF8StringEncoding];
+    [fileHandle writeData:sringData];
+    [fileHandle closeFile];//关闭文件
+    
+    //删除操作
+    BOOL we = [fileMng removeItemAtPath:textFile error:nil];
+    if(we){
+        NSLog(@"文件删除成功");
+    }else{
+        NSLog(@"文件删除失败");
+    }
+//    //获取Library路径
+//    NSArray *libPaths = NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES);
+//    NSLog(@"libPaths = %@",libPaths);
+//    //获取Tmp路径
+//    NSString *tmpPath = NSTemporaryDirectory();
+//    NSLog(@"tmpPath = %@",tmpPath);
+//    //获取路径的各个部分
+//    NSArray *arrpath = [tmpPath pathComponents];
+//    NSLog(@"arrpath = %@",arrpath);
+//    //删除路径的最后一个部分
+//    NSString *string = [tmpPath stringByDeletingLastPathComponent];
+//    NSLog(@"string = %@",string);
+//    //在路径后添加一个部分
+//    NSString *filepath = [tmpPath stringByAppendingString:@"name.text"];
+//    NSLog(@"filepath = %@",filepath);
 }
 
 
